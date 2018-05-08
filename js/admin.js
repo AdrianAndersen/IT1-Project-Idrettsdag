ac01cd005397798134020a9b5a38987522598666
Adrian Arthur Andersen <adrian.arthur.andersen@gmail.com>
2018-05-08 09:44:47 +0200

    la til admin forms og admin.js

---
+++ js/admin.js
@@ -0,0 +1,11 @@
+// Henter referansene til databasene
+const database = firebase.database();
+const idretter = database.ref("idretter/");
+const skoler = database.ref("skoler/");
+const lag = database.ref("lag/");
+// Henter HTML-elementer fra siden
+const 
+
+function nyIdrett(event) {
+
+}
--- js/registrering.js
+++ js/registrering.js
@@ -1,1 +1,31 @@
-// JavaScript source code
+// Henter referansene til databasene
+const database = firebase.database();
+const idretter = database.ref("idretter/");
+const skoler = database.ref("skoler/");
+const lag = database.ref("lag/");
+// Henter HTML-elementer fra siden
+const skjemaRegistrering = document.getElementById("skjemaRegistrering");
+const inpLagnavn = document.getElementById("inpLagnavn");
+const selSkole = document.getElementById("selSkole");
+const selKlasse = document.getElementById("selKlasse");
+const selIdrett = document.getElementById("selIdrett");
+
+
+function nyttLag (event) {
+    event.preventDefault();
+    let lagnavn = inpLagnavn.value;
+    let skole = selSkole.value;
+    let klasse = selKlasse.value;
+    let idrett = selIdrett.value;
+    lag.push(
+        {
+            "lagnavn": lagnavn,
+            "skole": skole,
+            "klasse": klasse,
+            "idrett": idrett 
+        }
+    );
+    inpLagnavn.value = "";
+}
+// Lyttefunksjoner
+skjemaRegistrering.onsubmit = nyttLag;
