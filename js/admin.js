6e63ed1389305519b602a0e72b2da08d80e9aede
Adrian Arthur Andersen <adrian.arthur.andersen@gmail.com>
2018-05-10 16:22:41 +0200

    Muchas cosas

--- js/admin.js
+++ js/admin.js
@@ -4,8 +4,20 @@ const idretter = database.ref("idretter/");
 const skoler = database.ref("skoler/");
 const lag = database.ref("lag/");
 // Henter HTML-elementer fra siden
-const 
+const skjemaNyIdrett = document.getElementById("skjemaNyIdrett");
+const skjemaEndreIdretter = document.getElementById("skjemaEndreIdretter");
+const selIdrettAdmin = document.getElementById("selIdrettAdmin");
+const skjemaEndreSkoler = document.getElementById("skjemaEndreSkoler");
+const selSkoleAdmin = document.getElementById("selSkoleAdmin");
+const skjemaEndreLag = document.getElementById("skjemaEndreLag");
+const selLagAdmin = document.getElementById("selLagAdmin");
 
-function nyIdrett(event) {
+const knappNyIdrett = document.getElementById("knappNyIdrett");
 
+function visSkjemaNyIdrett(event) {
+    console.log("Viser skjema");
+    skjemaNyIdrett.style.display = "block";
 }
+
+// Lyttefunskjoner
+knappNyIdrett.addEventListener("click", visSkjemaNyIdrett);
